1e4290d09289190c6bb50d1eac4880261b43bd57
Ajibola Vincent <ajibolavincent@gmail.com>
2020-01-01 23:08:00 -0600

    Add files via upload

--- ConvNet.py
+++ ConvNet.py
@@ -1,47 +1,55 @@
 import torch.nn as nn
 
-N_img_height = 66
-N_img_width = 220
-N_img_channels = 3
-inputShape = (N_img_height, N_img_width, N_img_channels)
+""" 
+A class implementing the structure of the end to end learning CNN developed by NVIDIA
+https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
 
-
-nn.Conv2d(N_img_channels, 24, kernel_size=5, stride=2)
-# normalization    
- # perform custom normalization before lambda layer in network
-#model.add(Lambda(lambda x: x/ 127.5 - 1, input_shape = inputShape))
-#nn.Conv2d(24, 36, 5)
+Methods
+-------
+forward(input)
+    Runs the forward pass on the CNN
+"""
 
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(24, 36, kernel_size=5, stride=2),
+             nn.Conv2d(3, 24, kernel_size=5, stride=2),
             nn.ReLU())
         self.layer2 = nn.Sequential(
+            nn.Conv2d(24, 36, kernel_size=5, stride=2),
+            nn.ReLU())
+        self.layer3 = nn.Sequential(
             nn.Conv2d(36, 48, kernel_size=5, stride=2),
             nn.ELU())
-        self.layer3 = nn.Sequential(
+        self.layer4 = nn.Sequential(
             nn.Conv2d(48, 64, kernel_size=5, stride=2),
             nn.ELU())
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(64, 32, kernel_size=3, stride=1),
-            nn.ELU())
         self.layer5 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, stride=1),
-            nn.ReLU())
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ELU())
         self.dropout = nn.Dropout(0.5) 
-        self.fc1 = nn.Linear(x,100)
+        self.fc1 = nn.Linear(64*1*18,100)
         self.fc2 = nn.Linear(100, 50)
         self.fc3 = nn.Linear(50,10)
         self.fc4 = nn.Linear(10, 1)
 
     def forward(self, input):
+        """Does the forward pass for the CNN
+        Parameters
+        ----------
+        input : tensor
+            The tensor representing the image in the batch 
+            
+        Returns
+        ------
+        outout: 
+            This is the CNN model 
+        """
         output = self.layer1(input)
         output = self.layer2(output)
         output = self.layer3(output)
         output = self.layer4(output)
         output = self.layer5(output)
-        return output
 
-#self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
+        return output
